b07fe3e92bad5e56b90fbb1226143722edae0bb1
Kw2x <bhen2014@gmail.com>
2024-01-28 14:15:02 -0500

    logic for finding area of shapes and for inputting c/s before it runs.

--- CalculateAreaApp/src/AreaApp.java
+++ CalculateAreaApp/src/AreaApp.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 
+
 public class AreaApp{
 	
 	public static void main(String[] args) {
@@ -10,17 +11,30 @@ public class AreaApp{
 		
 		System.out.println("Please enter two numbers to find the area");
 		
+		//String c = myObj.next().charAt(1);
+
+		char c = myObj.next().charAt(1);// 
+		char s = myObj.next().charAt(1);//
+
+		
 		double input1 = myObj.nextDouble();
-		
 		double input2 = myObj.nextDouble();
+		double circleArea = (input1 * input2 )*Math.PI;
 		
-		double total = input1 + input2;
+
+		/*if(myObj.equals('c')) {
+			double circleArea = (input1 * input2 )*Math.PI;
+			System.out.println("The area of the circle is " + circleArea);
+		}*/
 		
-		System.out.println(total);
+		//System.out.println("The area is " + c + circleArea); does nothing 
+		System.out.println("The area is " + circleArea);
+
 		
 		myObj.close(); // This closes the scanner class
 		
 	}
 }
+
 
 
